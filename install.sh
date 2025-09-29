8f56e2042663f46cb22002a6eb65ab1c8e164bb6
Apoorv Malik <1998apoorvmalik@gmail.com>
2025-09-29 14:58:37 -0400

    add install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ set -e
 
 # initialize and update submodules
 git submodule update --init --recursive
+cd external
+
+# setup RNA Eval
+cd rna-eval
+chmod +X ./setup_env.sh
 
 # make build script executable
 chmod +x ./build.sh
